cf59795a6e88a76a2d7dfedd12127c1153d4d6b4
Harsha Munisamy <HarshaM@clarium.tech>
2025-05-26 14:46:08 +0530

    Checking git is working in vscode

--- src/demo/sum.java
+++ src/demo/sum.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class sum {
 
 	public static void main(String args[]) {
-		int a,b;
+		int a,b,d;
 		
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter a:");
